68107bed3378f7d7eb08c82fd95f1fec2aea5e04
PETEPEtrek <senatoroff.p@gmail.com>
2023-03-19 12:28:24 +0300

    March 19

---
+++ nineteen_march_daily_211.py
@@ -0,0 +1,37 @@
+class WordDictionary:
+
+    def __init__(self):
+        self.children = [None]*26
+        self.is_end = False
+        
+
+    def addWord(self, word: str) -> None:
+        cur = self
+        for c in word:
+            if cur.children[ord(c) - ord('a')] == None:
+                cur.children[ord(c) - ord('a')] = WordDictionary()
+            cur = cur.children[ord(c) - ord('a')]
+        
+        cur.is_end = True
+        
+
+    def search(self, word: str) -> bool:
+        cur = self
+        for i in range(len(word)):
+            c = word[i]
+            if c == '.':
+                for ch in cur.children:
+                    if ch != None and ch.search(word[i+1:]): return True
+                return False
+            
+            if cur.children[ord(c) - ord('a')] == None: return False
+            cur = cur.children[ord(c) - ord('a')]
+        
+        return cur != None and cur.is_end
+        
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
